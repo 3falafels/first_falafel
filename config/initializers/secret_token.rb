9f9b65bc30a0a421fdf0c14da4ef55cc92a62c8c
unknown <Administrator@dell.(none)>
2012-01-15 19:49:25 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Falafal::Application.config.secret_token = '1236fcc41fbb8bc866ae08b7aff436fdf29cf584e20e21bb04233cf9ae923c5e17d96f7c316925328ab8af8fe73a4c34b5ea4436b1df5660008a57ba6df69160'
